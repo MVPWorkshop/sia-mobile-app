9fb19221894e4ab9d5a950e295c17b6e9e16cac4
Filip Pajic <fica.pajic@gmail.com>
2020-09-21 21:30:06 +0200

    Created wallet screen

--- src/components/Screens/CouponsScreen/couponsScreen.tsx
+++ src/components/Screens/CouponsScreen/couponsScreen.tsx
@@ -1,54 +1,14 @@
-import React, { useEffect } from 'react';
+import React  from 'react';
 import ScreenLayout from '../../layouts/ScreenLayout/screenLayout';
 import Typography from '../../atoms/Typography/typography';
 import { EPoppins } from '../../../shared/hooks/usePoppins.hook';
 import { View } from 'react-native';
 import { mb } from '../../../shared/styles/util.styles';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../../redux/redux.types';
-import { getColor, keys } from '../../../shared/utils/common.util';
-import Coupon from '../../organisms/Coupon/coupon';
-import { IColorTheme } from '../../../shared/types/common.types';
-import { EColors } from '../../../shared/styles/variables.styles';
 import { RouterScreenProps } from '../../../shared/types/router.types';
 import styles from './couponsScreen.styles';
+import UserCoupons from '../../organisms/UserCoupons/userCoupons';
 
 const CouponsScreen: React.FC<RouterScreenProps.ICouponsScreenProps> = () => {
-
-  const coupons = useSelector<RootState, RootState['wallet']['coupons']>(state => state.wallet.coupons);
-
-  useEffect(() => {
-    console.log('changes')
-  }, [coupons])
-
-  const renderCoupons = () => {
-    const colorThemes: IColorTheme[] = [
-      {
-        normal: getColor(EColors.PINK),
-        lighter: getColor(EColors.PINK_LIGHTER)
-      },
-      {
-        normal: getColor(EColors.GREEN),
-        lighter: getColor(EColors.GREEN_LIGHTER)
-      },
-      {
-        normal: getColor(EColors.BLUE),
-        lighter: getColor(EColors.BLUE_LIGHTER)
-      }
-    ]
-
-    return keys(coupons).map((couponId, index) => {
-      return (
-        <View style={mb(4)} key={couponId + index}>
-          <Coupon
-            coupon={coupons[couponId]}
-            colorTheme={colorThemes[index % 3]}
-          />
-        </View>
-      )
-    })
-  }
-
   return (
     <ScreenLayout
       safeArea={false}
@@ -60,7 +20,7 @@ const CouponsScreen: React.FC<RouterScreenProps.ICouponsScreenProps> = () => {
           Coupons
         </Typography>
       </View>
-      {renderCoupons()}
+      <UserCoupons/>
     </ScreenLayout>
   )
 }
--- src/components/Screens/WalletScreen/walletScreen.tsx
+++ src/components/Screens/WalletScreen/walletScreen.tsx
@@ -1,12 +1,113 @@
-import React from 'react'
-import { Text } from 'react-native';
+import React, { Fragment, useState } from 'react'
+import { Image, View } from 'react-native';
 import { RouterScreenProps } from '../../../shared/types/router.types';
 import ScreenLayout from '../../layouts/ScreenLayout/screenLayout';
+import styles from './walletScreen.styles';
+import { mb } from '../../../shared/styles/util.styles';
+import Typography from '../../atoms/Typography/typography';
+import { EPoppins } from '../../../shared/hooks/usePoppins.hook';
+import { EColors } from '../../../shared/styles/variables.styles';
+import { Images } from '../../../shared/constants/images.constants';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../redux/redux.types';
+import Divider from '../../atoms/Divider/divider';
+import UserCoupons from '../../organisms/UserCoupons/userCoupons';
+import TextInput from '../../atoms/TextInput/textInput';
+import useForm from '../../../shared/hooks/useForm.hook';
+import supportForm from '../../../shared/forms/support.form';
+import Button from '../../atoms/Button/button';
 
 const WalletScreen: React.FC<RouterScreenProps.IWalletScreenProps> = () => {
+
+  const userTokens = useSelector<RootState, RootState['wallet']['userTokens']>(state => state.wallet.userTokens);
+  const { isFormValid, formState, updateInput } = useForm(supportForm);
+  const [displaySupport, setDisplaySupport] = useState(true)
+
   return (
-    <ScreenLayout>
-      <Text>Wallet screen</Text>
+    <ScreenLayout
+      scroll={true}
+      safeArea={false}
+      style={styles.walletScreen}
+    >
+      <View style={mb(4)}>
+        <Typography fontFamily={EPoppins.SemiBold} fontSize={24}>
+          My Tokens
+        </Typography>
+      </View>
+
+      <View style={{
+        alignItems: 'center',
+        alignSelf: 'stretch'
+      }}>
+        <Typography fontSize={14} color={EColors.GRAY_DARKEST}>
+          NUMBER OF MY TOKENS
+        </Typography>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Image
+            source={Images.coinsIcon}
+          />
+          <Typography fontFamily={EPoppins.SemiBold} color={EColors.BLUE} fontSize={30}>
+            {userTokens.current}
+          </Typography>
+        </View>
+
+        <View style={{flexDirection: 'row', marginTop: 20}}>
+          <View style={{flex: 1, alignItems: 'center', marginRight: 8}}>
+            <Typography fontSize={14} color={EColors.GRAY_DARKEST}>
+              YESTERDAY EARNINGS
+            </Typography>
+            <Typography fontFamily={EPoppins.SemiBold} fontSize={30}>
+              {userTokens.yesterdayEarned}
+            </Typography>
+          </View>
+          <View style={{flex: 1, alignItems: 'center'}}>
+            <Typography fontSize={14} color={EColors.GRAY_DARKEST}>
+              LAST MONTH EARNINGS
+            </Typography>
+            <Typography fontFamily={EPoppins.SemiBold} fontSize={30}>
+              {userTokens.lastMonthEarned}
+            </Typography>
+          </View>
+        </View>
+      </View>
+      <Divider/>
+      <View style={mb(4)}>
+        <Typography fontFamily={EPoppins.SemiBold} fontSize={24}>
+          My Coupons
+        </Typography>
+      </View>
+      <UserCoupons/>
+      <View style={mb(4)}>
+        <Typography fontFamily={EPoppins.SemiBold} fontSize={24}>
+          Contact Support
+        </Typography>
+      </View>frd
+      { displaySupport ?
+        <Fragment>
+          <TextInput
+            name={formState.message.inputName}
+            value={formState.message.value}
+            onChange={updateInput}
+            error={formState.message.error?.formattedMessage}
+            placeholder={'Send a message to our team'}
+            multiline={true}
+            containerStyle={mb(4)}
+          />
+          <Button
+            onClick={() => setDisplaySupport(false)}
+            disabled={!isFormValid}
+            style={{
+              alignSelf: 'flex-end',
+              width: 170
+            }}
+          >
+            Send
+          </Button>
+        </Fragment> :
+        <Typography color={EColors.GREEN}>
+          Your message was successfully sent!
+        </Typography>
+      }
     </ScreenLayout>
   )
 }
